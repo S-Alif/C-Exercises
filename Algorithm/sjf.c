a97159fd80e6da6c83ef57b90c8fcb4fd1eb6894
Alif <79266506+S-Alif@users.noreply.github.com>
2023-09-13 23:15:48 +0600

    new

--- Algorithm/sjf.c
+++ Algorithm/sjf.c
@@ -3,73 +3,112 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+int n;
 
 // Structure to represent a process
-struct Process {
+typedef struct process {
+    char process[10];
     int arrival_time;
     int burst_time;
     int completion_time;
     int waiting_time;
     int turnaround_time;
+
     bool completed;
-};
+}process;
+
+process *processes;
+
+int calculate(int shortest_job, int current_time){
+
+    processes[shortest_job].completion_time = current_time + processes[shortest_job].burst_time;
+    processes[shortest_job].turnaround_time = processes[shortest_job].completion_time - processes[shortest_job].arrival_time;
+    processes[shortest_job].waiting_time = processes[shortest_job].turnaround_time - processes[shortest_job].burst_time;
+
+    current_time = processes[shortest_job].completion_time;
+    processes[shortest_job].completed = true;
+
+    return current_time;
+}
 
 // Function to find the process with the shortest burst time in the ready queue
-int findShortestJob(struct Process processes[], int n, int current_time) {
-    int shortest_job_index = -1;
+int shortestJob(process array[], int current_time) {
+    int shortest_job = -1;
     int shortest_job_time = -1;
 
     for (int i = 0; i < n; i++) {
-        if (!processes[i].completed && processes[i].arrival_time <= current_time) {
-            if (shortest_job_index == -1 || processes[i].burst_time < shortest_job_time) {
-                shortest_job_index = i;
-                shortest_job_time = processes[i].burst_time;
+        if (!array[i].completed && array[i].arrival_time <= current_time) {
+            if (shortest_job == -1 || array[i].burst_time < shortest_job_time) {
+                shortest_job = i;
+                shortest_job_time = array[i].burst_time;
             }
         }
     }
 
-    return shortest_job_index;
+    return shortest_job;
+}
+
+// enter data
+void enterData(int indexPosition){
+  printf("enter process name : ");
+  scanf("%s", processes[indexPosition].process);
+
+  printf("enter burst time : ");
+  scanf("%d", &processes[indexPosition].burst_time);
+
+  printf("enter arrival time : ");
+  scanf("%d", &processes[indexPosition].arrival_time);
+
+  printf("\n----------------------------\n\n");
 }
 
 int main() {
-    int n;
+  process sorted;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    struct Process processes[n];
+    processes = malloc(sizeof(process) * n);
 
     // Input process details
     for (int i = 0; i < n; i++) {
-        printf("Enter arrival time for Process %d: ", i + 1);
-        scanf("%d", &processes[i].arrival_time);
-        printf("Enter burst time for Process %d: ", i + 1);
-        scanf("%d", &processes[i].burst_time);
-        processes[i].completed = false;
+      enterData(i);
+    }
+    int current_time = processes[0].arrival_time;
+
+    for (int i = 0; i < n; i++){
+      if(processes[i].arrival_time < current_time){
+        current_time = processes[i].arrival_time;
+      }
     }
 
-    int current_time = 0;
 
     // Execute processes
     while (true) {
-        int shortest_job_index = findShortestJob(processes, n, current_time);
-
-        if (shortest_job_index == -1)
-            break;
-
-        processes[shortest_job_index].completion_time = current_time + processes[shortest_job_index].burst_time;
-        processes[shortest_job_index].turnaround_time = processes[shortest_job_index].completion_time - processes[shortest_job_index].arrival_time;
-        processes[shortest_job_index].waiting_time = processes[shortest_job_index].turnaround_time - processes[shortest_job_index].burst_time;
-
-        current_time = processes[shortest_job_index].completion_time;
-        processes[shortest_job_index].completed = true;
+        int shortest_job = shortestJob(processes, current_time);
+        if (shortest_job == -1){
+          break;
+        }
+        current_time = calculate(shortest_job, current_time);
     }
 
+    for (int i = 0; i < n; i++){
+    for(int j = i+1; j < n; j++){
+      if(processes[i].completion_time > processes[j].completion_time){
+        sorted = processes[i];
+        processes[i] = processes[j];
+        processes[j] = sorted;
+      }
+    }
+  }
+
     // Display results
-    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
+    printf("\nprocess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, processes[i].arrival_time,
+        printf("%s\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].process, processes[i].arrival_time,
                processes[i].burst_time, processes[i].completion_time,
-               processes[i].waiting_time, processes[i].turnaround_time);
+               processes[i].turnaround_time, processes[i].waiting_time);
     }
 
     return 0;
